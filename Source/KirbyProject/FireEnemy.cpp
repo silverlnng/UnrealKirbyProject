ed427d95ffcc669eced631cae95c72d36015956e
Subin <80036437+so0oppy@users.noreply.github.com>
2024-06-24 17:30:24 +0900

    [feat] c++ 코드 추가

--- Source/KirbyProject/FireEnemy.cpp
+++ Source/KirbyProject/FireEnemy.cpp
@@ -1,24 +1,22 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "FireEnemy.h"
 
-// Sets default values
 AFireEnemy::AFireEnemy()
 {
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	Root = CreateDefaultSubobject<USceneComponent>(TEXT("Root"));
+	SetRootComponent(Root);
+
+	FireMesh = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("FireMesh"));
+	FireMesh->SetupAttachment(Root);
 }
 
-// Called when the game starts or when spawned
 void AFireEnemy::BeginPlay()
 {
 	Super::BeginPlay();
 	
 }
 
-// Called every frame
 void AFireEnemy::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
--- Source/KirbyProject/FireEnemy.h
+++ Source/KirbyProject/FireEnemy.h
@@ -1,5 +1,3 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
@@ -11,16 +9,18 @@ class KIRBYPROJECT_API AFireEnemy : public AActor
 {
 	GENERATED_BODY()
 	
+private:
+	UPROPERTY(VisibleAnywhere)
+		USceneComponent* Root;
+	UPROPERTY(EditDefaultOnly)
+		USkeletalMeshComponent* FireMesh;
 public:	
-	// Sets default values for this actor's properties
 	AFireEnemy();
 
 protected:
-	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
 public:	
-	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
 };
