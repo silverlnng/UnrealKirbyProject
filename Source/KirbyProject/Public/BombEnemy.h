6aefff16136a66a650a7511fe2e2c1fa6b331e86
KimSangKyung <shamong123@naver.com>
2024-07-05 17:15:37 +0900

    Revert "Merge branch 'master' of https://github.com/silverlnng/UnrealKirbyProject into SangGang"
    This reverts commit 850d8bcc652943cefb65418ee9125110dbe6c2ba, reversing
    changes made to ddb4b35cde6c4a90b96b03b4328629bdac7490dd.

--- Source/KirbyProject/Public/BombEnemy.h
+++ Source/KirbyProject/Public/BombEnemy.h
@@ -29,9 +29,6 @@ private:
     UPROPERTY(EditDefaultsOnly, Category = "BombEnemy")
     TSubclassOf<class AActor> CoinClass;  // 코인 클래스 선언
 
-    UPROPERTY(VisibleAnywhere, Category = Collision)
-    class UCapsuleComponent* DetectionCapsule;
-
     FTimerHandle BombTimerHandle;
 
     APawn* PlayerPawn;
@@ -52,11 +49,6 @@ public:
 
     UFUNCTION(BlueprintCallable)
     void OnHit(float Damage);  // Enemy가 공격을 받을 때 호출되는 함수
-
-    UFUNCTION()
-    void OnOverlapBegin(class UPrimitiveComponent* OverlappedComp, class AActor* OtherActor, 
-                        class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, 
-                        const FHitResult & SweepResult);
 
 private:
     void CheckBombCondition();
